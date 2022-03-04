bba44d037bd4c3bb46743f0f27ee47d8989d1985
Andrew Bradley <cspotcode@gmail.com>
2022-03-03 23:28:18 -0500

    switch to cjs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const isEscaped = (jsonString, quotePosition) => {
 	return Boolean(backslashCount % 2);
 };
 
-exports.default = stripJsonComments;
+module.exports = stripJsonComments;
 function stripJsonComments(jsonString, {whitespace = true} = {}) {
 	if (typeof jsonString !== 'string') {
 		throw new TypeError(`Expected argument \`jsonString\` to be a \`string\`, got \`${typeof jsonString}\``);
